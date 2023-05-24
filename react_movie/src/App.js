96291313b2ac0482d8f37628c0a1ede297d877be
jrim <comfortindex@naver.com>
2023-05-24 12:51:39 +0900

    feat: coin

--- react_movie/src/App.js
+++ react_movie/src/App.js
@@ -1,38 +1,57 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [budget, setBudget] = useState(0);
 
-  const onChange = (event) => setToDo(event.target.value);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false) 
+      }); 
+  }, []);
+
+  const onChange = (event) => setBudget(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentToDos) => [toDo, ...currentToDos]);
-    setToDo("");
-    console.log(toDos);
+    setBudget();
+    console.log(budget);
   }
-  
+  const onSelect = (event) => {
+    console.log(event.target.value);
+  }
+
   return (
     <div>
-      <h2>my todos: {toDos.length}</h2>
+      <h3>the coins ({loading ? "" : coins.length})</h3>
+      <hr/>
+      {loading ? <strong>loading...</strong> : 
+        <select>
+          <option>select coin</option>
+          {coins.map((coin) => 
+            <option 
+              key={coin.id}
+              value={coin.quotes.USD.price}
+              symbol={coin.symbol}
+              onSelect={onSelect}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>)}
+        </select>
+      }
+      <br/>
+      <br/>
       <form onSubmit={onSubmit}>
-        <input 
-          type="text" 
-          placeholder="todo" 
-          value={toDo}
+        <input
+          type="text"
+          placeholder="USD"
+          value={budget}
           onChange={onChange}
         />
-        <button>add todo</button>
+        <button>submit</button>
       </form>
-      <hr />
-      <ul>
-        {toDos.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
-      </ul>
     </div>
   );
 }
